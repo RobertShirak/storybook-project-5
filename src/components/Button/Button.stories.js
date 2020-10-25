05548d413420d4b77339186c139530cb9a270fcd
Robert Shirak <bcshirak1@gmail.com>
2020-10-25 14:05:55 -0400

    moved

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,5 +3,31 @@ import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
+function helloWorld(){
+    alert("Button Component Executed props.onClick")
+}
+
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button 
+    label="Primary"
+    onClick={helloWorld} 
+    type="primary" />) 
+    .add('Danger', () => <Button 
+    label="danger" 
+    type="danger" />) 
+    .add('Success', () => <Button 
+    label="success" 
+    type="success" />)
+    .add('Warning', () => <Button 
+    label="warning" 
+    type="warning" />)
+    .add('Large Primary', () => <Button
+        label="Large Primary Button"
+        type="primary"
+        large
+    />)
+    .add('Large Danger', () => <Button
+        label="Large Danger Button"
+        type="danger"
+        large
+    />)
